4c42212e4cce189f94589cfbec878cc436aa40b8
accountCoddy <account.coddy@mail.ru>
2023-04-30 09:10:26 +0300

    1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = 3000
-
+let users = []
 
 app.use(express.static('static'))
 
@@ -16,8 +16,25 @@ io.on('connection', socket=>{
     socket.on('chatMessage', msg =>{
         io.emit('chatMessage', msg)
     })
+
+    socket.on('addUser', msg =>{
+        socket.user = msg
+        users.push(msg)
+        updateUsers()
+    })
+
+    socket.on('connect', ()=>{
+        updateUsers()
+    })
+
+    socket.on('getUser', ()=>{
+        io.emit('getUser', users)
+    })
 })
 
+function updateUsers(){
+    io.emit('update', users)
+}
 
 
 
--- static/script.js
+++ static/script.js
@@ -3,6 +3,8 @@ const form = document.querySelector('#message_form')
 const input_message = document.querySelector('#message')
 const all_messages = document.querySelector('#all_messages')
 const input_name = document.querySelector('#name')
+let users = []
+const userHolder = document.querySelector('.users')
 
 
 form.addEventListener('submit', e =>{
@@ -12,6 +14,9 @@ form.addEventListener('submit', e =>{
         name: input_name.value,
         message: input_message.value,
     })
+
+    socket.emit('addUser', input_name.value)
+
     input_message.value = ''
 })
 
@@ -20,3 +25,32 @@ socket.on('chatMessage', msg =>{
     newMessage.textContent = `${msg.name}: ${msg.message}`
     all_messages.append(newMessage)
 })
+
+
+socket.on('update', msg =>{
+    updateListUser(msg)
+})
+
+socket.on('disconnect', (msg)=>{
+    console.log(msg)
+})
+
+socket.on('connect', msg =>{
+    socket.emit('getUser')
+})
+
+socket.on('getUser', users =>{
+    updateListUser(users)
+})
+
+
+function updateListUser(listUsers){
+    users = listUsers
+    console.log(users)
+    userHolder.innerHTML = ''
+    users.forEach(user =>{
+        let newUser = document.createElement('li')
+        newUser.textContent = user
+        userHolder.append(newUser)
+    })
+}
